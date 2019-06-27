eeb5906185b303daafa7c0ba38956ad56773a128
Michael Nguyen <sir.michaelmn@gmail.com>
2019-06-26 20:19:24 -0700

    optimized of h_887, courtesy of https://leetcode.com/problems/super-egg-drop/discuss/159079/Python-DP-from-kn2-to-knlogn-to-kn

--- h_887.py
+++ h_887.py
@@ -7,52 +7,59 @@ class Solution:
         self.total_r += 1
         # print(K, N)
 
-        if N == 0 or N == 1:
+        if K == 0:
+            return 0
+
+        if N == 0 or N == 1 or K == 1:
             return N
 
-        else:
-            half = math.ceil(N/2)
-            if K > half:
-                KK = half
+        if (K, N) in self.dp:
+            return self.dp[(K,N)]
+
+        low = 2
+        high = N
+        vv = N
+        while True:
+            mid = math.ceil((high+low) / 2)
+            bottom_N = mid - 1
+            top_N = N - mid
+
+            top_sol = self.superEggDropHelper(K, top_N)
+            bott_sol = self.superEggDropHelper(K - 1, bottom_N)
+
+            v = 1 + max(top_sol, bott_sol)
+            if v < vv:
+                vv = v
+
+            if high == low or top_sol == bott_sol:
+                break
+
+            if top_sol > bott_sol:
+                low = mid
             else:
-                KK = K
+                if mid == high:
+                    high = low
+                else:
+                    high = mid
 
-            if (KK, N) in self.dp:
-                return self.dp[(KK,N)]
 
-            vv = N
-            if K >= 2:
-                r = range(1, half+1)
-            else:
-                r = [KK]
-
-            for i in r:
-                bottom_N = i - 1
-                top_N = N - i
-
-                v = 1 + max(
-                    self.superEggDropHelper(KK - 1, bottom_N),
-                    self.superEggDropHelper(KK, top_N)
-                )
-
-                if v < vv:
-                    vv = v
-
-            self.dp[(KK,N)] = vv
-            print(self.dp)
-            return vv
+        self.dp[(K,N)] = vv
+        # print(self.dp)
+        return vv
 
     def superEggDrop(self, K: int, N: int) -> int:
         self.total_r = 0
         v = self.superEggDropHelper(K, N)
-        print("total: " + str(self.total_r))
+        # print("total: " + str(self.total_r))
         return v
 
 s = Solution()
 assert(s.superEggDrop(2,9) == 4)
-# assert(s.superEggDrop(2,7) == 4)
-# assert(s.superEggDrop(1,3) == 3)
-# assert(s.superEggDrop(2,6) == 3)
-# assert(s.superEggDrop(1,2) == 2)
-# assert(s.superEggDrop(3,14) == 4)
-# assert(s.superEggDrop(100,10000))
+assert(s.superEggDrop(2,11) == 5)
+assert(s.superEggDrop(3,11) == 4)
+assert(s.superEggDrop(2,7) == 4)
+assert(s.superEggDrop(1,3) == 3)
+assert(s.superEggDrop(2,6) == 3)
+assert(s.superEggDrop(1,2) == 2)
+assert(s.superEggDrop(3,14) == 4)
+assert(s.superEggDrop(100,10000))
