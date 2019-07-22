0cfb78343905688b1e86f50722053f52b88aac71
Michael Nguyen <sir.michaelmn@gmail.com>
2019-07-22 00:05:15 -0700

    finish LFU cache

--- h_460.py
+++ h_460.py
@@ -74,7 +74,7 @@ class LFUCache:
     def add_inv(self, _level, key):
         # print("add_inv", _level, key)
         if _level not in self.d_count_inv.children:
-            if _level == 1:
+            if self.d_count_inv.head and _level < self.d_count_inv.head.val:
                 self.insert_0_children(self.d_count_inv, _level)
             else:
                 self.append_children(self.d_count_inv, _level)
@@ -150,6 +150,32 @@ class LFUCache:
         self.least_used_i = self.d_count_inv.head.val
         self.dbg_print("###\n")
 
+cache = LFUCache(3);
+cache.put(1, 1);
+cache.put(2, 2);
+cache.put(3, 3);
+cache.get(1);
+cache.get(2);
+cache.get(3);
+cache.get(1);
+cache.get(2);
+cache.get(3);
+cache.get(1);
+cache.get(2);
+cache.get(3);
+cache.get(1);
+cache.get(2);
+cache.get(3);
+cache.get(1);
+cache.get(2);
+cache.get(3);
+cache.get(1);
+cache.get(2);
+cache.get(3);
+cache.put(4, 4);
+cache.get(4);
+cache.put(5, 5);
+
 # cache = LFUCache(2);
 #
 # cache.put(1, 1);
@@ -169,16 +195,19 @@ class LFUCache:
 # cache.put(0, 0);
 # cache.get(0);
 
-i_0 = ["LFUCache","put","put","get","get","get","put","put","get","get","get","get"]
-i_1 = [[3],[2,2],[1,1],[2],[1],[2],[3,3],[4,4],[3],[2],[1],[4]]
-
-c = None
-for i in range(len(i_0)):
-    fu = i_0[i]
-    inp = i_1[i]
-    if fu == "LFUCache":
-        c = LFUCache(*inp)
-    elif fu == "put":
-        c.put(*inp)
-    elif fu == "get":
-        c.get(*inp)
+# i_0 = ["LFUCache","put","put","get","get","get","put","put","get","get","get","get"]
+# i_1 = [[3],[2,2],[1,1],[2],[1],[2],[3,3],[4,4],[3],[2],[1],[4]]
+# c = None
+# print(len(i_0))
+# for i in range(len(i_0)):
+#     fu = i_0[i]
+#     inp = i_1[i]
+#     if fu == "LFUCache":
+#         c = LFUCache(*inp)
+#     elif fu == "put":
+#         c.put(*inp)
+#     elif fu == "get":
+#         o = c.get(*inp)
+#         if o != e[i]:
+#             print(i, o, e[i])
+#             raise
