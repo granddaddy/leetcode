b378926bd3dc6f6104f74c8100971a13b580548f
Michael Nguyen <sir.michaelmn@gmail.com>
2019-08-15 21:29:45 -0700

    fix to use real union find

--- m_547.py
+++ m_547.py
@@ -1,8 +1,14 @@
+_print = print
+
+def print(msg):
+    if True:
+        _print(msg)
+
 class Solution:
     def findCircleNum(self, M):
-        for _m in M:
-            print(_m)
-        print("####")
+        # for _m in M:
+        #     print(_m)
+        # print("####")
         N = len(M)
 
         if not N:
@@ -10,30 +16,26 @@ class Solution:
 
         f_circle = list(range(N))
 
-        def set_f_circle(mn, i):
-            if f_circle[mn] == mn:
-                old = f_circle[i]
-                for j in range(N):
-                    if f_circle[j] == old:
-                        f_circle[j] = mn
-
+        def find(i):
+            if f_circle[i] == i:
+                return i
             else:
-                set_f_circle(f_circle[mn], i)
+                return find(f_circle[i])
+
+        def union(i, j):
+            f_circle[find(i)] = find(j)
 
         for i in range(N):
             for j in range(N):
-                if i == j:
-                    continue
-                if M[i][j] == 1:
-                    mn = min(f_circle[i], f_circle[j])
-                    set_f_circle(mn, i)
-                    set_f_circle(mn, j)
-                    print(mn, i, j)
-                    print(f_circle)
+                if i != j and M[i][j] == 1:
+                    union(i, j)
 
-        print(f_circle)
+        # print(f_circle)
         return sum(map(lambda i: 1 if f_circle[i] == i else 0, range(N)))
 
 s = Solution()
 M = [[1,0,0,1],[0,1,1,0],[0,1,1,1],[1,0,1,1]]
+print(s.findCircleNum(M))
+
+M = [[1,0,0,0,0,0,0,0,0,1,0,0,0,0,0],[0,1,0,1,0,0,0,0,0,0,0,0,0,1,0],[0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],[0,1,0,1,0,0,0,1,0,0,0,1,0,0,0],[0,0,0,0,1,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],[0,0,0,1,0,0,0,1,1,0,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0,0,0],[0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],[0,0,0,0,1,0,0,0,0,0,0,0,1,0,0],[0,1,0,0,0,0,0,0,0,0,0,0,0,1,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]]
 print(s.findCircleNum(M))
